cafcff3468d5584309feee9edb6510a4766bd0c1
RubenCastroCoding <RubenCastroCoding@gmail.com>
2022-07-03 20:22:01 -0400

    creating user model and added data to it

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true, trim: true },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+module.exports = User;
