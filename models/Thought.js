f6e3a1dd0ef9a75824f5f4d1b24845dcd3dbeba2
RubenCastroCoding <RubenCastroCoding@gmail.com>
2022-07-03 20:20:41 -0400

    creating thought model

---
+++ models/Thought.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+  createdAt: { type: Date, default: Date.now}
+});
