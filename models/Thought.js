ae6351cbc44947b17f61a62c38c555391e79e371
RubenCastroCoding <RubenCastroCoding@gmail.com>
2022-07-03 20:49:54 -0400

    creating controllers

--- models/Thought.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
-  createdAt: { type: Date, default: Date.now}
-});
